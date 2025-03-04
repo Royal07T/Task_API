d69edfe8a20a6ffb3e22090f32c336957c229f0e
Royal07T <timmyroyalty@gmail.com>
2025-03-04 22:45:23 +0100

    update

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Http\Controllers\Controller;
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        return response()->json(Task::all(), 200);
+    }
+
+    public function store(Request $request)
+    {
+        $task = Task::create($request->all());
+        return response()->json($task, 201);
+    }
+
+    public function show($id)
+    {
+        return response()->json(Task::findOrFail($id), 200);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $task = Task::findOrFail($id);
+        $task->update($request->all());
+        return response()->json($task, 200);
+    }
+
+    public function destroy($id)
+    {
+        Task::destroy($id);
+        return response()->json(null, 204);
+    }
+}
